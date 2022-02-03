b4f841e67cff1169f94a36cb633ab03718e77b0a
nadiafasani <nadia.fasani@samtrevano.ch>
2022-02-03 11:46:25 +0100

    prova acceletometro 2

--- docs/prova.js
+++ docs/prova.js
@@ -3,6 +3,12 @@ acl.addEventListener('reading', () => {
     console.log("Acceleration along the X-axis " + acl.x);
     console.log("Acceleration along the Y-axis " + acl.y);
     console.log("Acceleration along the Z-axis " + acl.z);
+
+
+    document.getElementById("x").value = acl.x;
+    document.getElementById("y").value = acl.y;
+    document.getElementById("z").value = acl.z;
+
 });
 
 acl.start();
