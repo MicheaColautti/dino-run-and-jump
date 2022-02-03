04820ef098f2fd675161cd6bba49054a7e3f3619
nadiafasani <nadia.fasani@samtrevano.ch>
2022-02-03 12:01:33 +0100

    prova acceletometro 5

--- docs/prova.js
+++ docs/prova.js
@@ -1,14 +1,39 @@
-let acl = new Accelerometer({ frequency: 60 });
-acl.addEventListener('reading', () => {
-    console.log("Acceleration along the X-axis " + acl.x);
-    console.log("Acceleration along the Y-axis " + acl.y);
-    console.log("Acceleration along the Z-axis " + acl.z);
+let accelerometer = null;
+try {
+    accelerometer = new Accelerometer({ referenceFrame: 'device' });
+    accelerometer.addEventListener('error', event => {
+        // Handle runtime errors.
+        if (event.error.name === 'NotAllowedError') {
+            // Branch to code for requesting permission.
+        } else if (event.error.name === 'NotReadableError') {
+            console.log('Cannot connect to the sensor.');
+        }
+    });
+
+    accelerometer.addEventListener('reading', () => {
+        console.log("Acceleration along the X-axis " + accelerometer.x);
+        console.log("Acceleration along the Y-axis " + accelerometer.y);
+        console.log("Acceleration along the Z-axis " + accelerometer.z);
 
 
-    document.getElementById("x").value = acl.x;
-    document.getElementById("y").value = acl.y;
-    document.getElementById("z").value = acl.z;
+        document.getElementById("x").value = accelerometer.x;
+        document.getElementById("y").value = accelerometer.y;
+        document.getElementById("z").value = accelerometer.z;
 
-});
+    });
 
-acl.start();
+
+
+    accelerometer.start();
+
+} catch (error) {
+    // Handle construction errors.
+    if (error.name === 'SecurityError') {
+        // See the note above about feature policy.
+        console.log('Sensor construction was blocked by a feature policy.');
+    } else if (error.name === 'ReferenceError') {
+        console.log('Sensor is not supported by the User Agent.');
+    } else {
+        throw error;
+    }
+}
