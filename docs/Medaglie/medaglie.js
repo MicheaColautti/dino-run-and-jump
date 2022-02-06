a17dd60ec3d15bda81f68a33e0f6b99693b2537a
unknown <thomas.sartini@samtrevano.ch>
2022-02-06 12:43:47 +0100

    algoritmo medaglie completo

--- docs/Medaglie/medaglie.js
+++ docs/Medaglie/medaglie.js
@@ -1,8 +1,23 @@
 
-
+/**
+ * Genera delle forme svg dalle dimensioni passate come parametro e ritorna 
+ * tutte le forme sottoforma di array di stringhe.
+ * 
+ * @param {number} x definisce la posizione x del punto in alto a sinistra dello
+ * spazio dove creare le forme
+ * 
+ * @param {number} y definisce la posizione y del punto in alto a sinistra dello
+ * spazio dove creare le forme
+ * 
+ * @param {number} d definisce la lunghezza del lato del quadrato (spazio) dove
+ * creare le forme
+ * 
+ * @returns {Array.<String>} ritorna un array di stringhe contenente le forme 
+ * svg nel formato html, dalla piu grande alla piu piccola.
+ */
 function createMedal(x, y, d){ 
     var medal = [];
-    var c = [x+d/2, y+d/2];
+    var c = [x+d/2, y+d/2]; 
     var len = d/Math.sqrt(2);
 
     medal.push('<circle cx="'+c[0]+'" cy="'+c[1]+'" r="'+d/2+'" />');
@@ -28,26 +43,29 @@ function createMedal(x, y, d){
     var x50 = xx+len/2;
     var y50 = yy+len/2;
 
-    medal.push('<circle cx="'+x75+'" cy="'+y25+'" r="'+len*0.05+'" />');
-    medal.push('<circle cx="'+x75+'" cy="'+y75+'" r="'+len*0.05+'" />');
-    medal.push('<circle cx="'+x25+'" cy="'+y25+'" r="'+len*0.05+'" />');
-    medal.push('<circle cx="'+x25+'" cy="'+y75+'" r="'+len*0.05+'" />');
-
-    medal.push('<polygon points="'+x35+','+y35+' '+x65+','+y35+' '+x50+','+x80+'" /> ');
     medal.push('<polygon points="'+x20+','+y50+' '+x50+','+(yy+len)+' '+x80+','+y50+' '+x50+','+yy+'" /> ');
+    medal.push('<polygon points="'+x35+','+y35+' '+x65+','+y35+' '+x50+','+x80+'" /> ');
 
     medal.push('<polygon points="'+x20+','+yy+' '+xx+','+y20+' '+xx+','+yy+'" />');
     medal.push('<polygon points="'+x80+','+yy+' '+(xx+len)+','+y20+' '+(xx+len)+','+yy+'" />');
     medal.push('<polygon points="'+xx+','+y80+' '+x20+','+(yy+len)+' '+xx+','+(yy+len)+'" />');
     medal.push('<polygon points="'+x80+','+(yy+len)+' '+(xx+len)+','+y80+' '+(xx+len)+','+(yy+len)+'" />');
+
+    medal.push('<circle cx="'+x75+'" cy="'+y25+'" r="'+len*0.05+'" />');
+    medal.push('<circle cx="'+x75+'" cy="'+y75+'" r="'+len*0.05+'" />');
+    medal.push('<circle cx="'+x25+'" cy="'+y25+'" r="'+len*0.05+'" />');
+    medal.push('<circle cx="'+x25+'" cy="'+y75+'" r="'+len*0.05+'" />');
+
     fillMedal(medal);
 }
 
 function drawMedal(medal){
     result = "";
-    result += setColor(medal[medal.length-1], "gold");
-    medal.splice(medal.length-1, medal.length-1); //rimuove l'ultimo elemento
-    medal.forEach(element => result += element);
+    for(var i = 0; i<medal.length; i++){
+        if(medal[i] != undefined && medal[i] != null){
+            result += medal[i];
+        }
+    }
     document.getElementById("svg").innerHTML = result;
 
 }
@@ -82,25 +100,17 @@ function fillMedal(medal){
     var result = [];
     var quantity = Math.floor(Math.random() * (medal.length-1))+1 ;
     var selectedMedals = [];
-    console.log(quantity);
+    result[0] = setColor(medal[0], "gold");
     for(var i = 0; i<quantity; i++){
         var num =  Math.floor(Math.random() * (medal.length-1)) + 1;
-        if(! selectedMedals.includes(num)){
+        if(!selectedMedals.includes(num)){
             selectedMedals.push(num);
             medal[num] = setColor(medal[num], getRandomColor());
-
-            if(medal[num].includes("rect")){
-                result.unshift(medal[num]);
-            }else{
-                result.push(medal[num]);
-            }
-           
-            
+            result[num] = medal[num];
         }else{
             quantity++;
         }
     }
-    result.push(medal[0]);
     drawMedal(result);
 
 }
