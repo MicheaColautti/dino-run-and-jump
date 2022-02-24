bb7949d17bac8aa9d39f8407db75bfaf0ccf39a1
Nadia Fasani <nadia.fasani@samtrevano.ch>
2022-02-24 10:53:03 +0100

    modificata provaLobby.html + lobby.js

--- docs/GUI telefono/lobby.js
+++ docs/GUI telefono/lobby.js
@@ -3,6 +3,10 @@ var id = localStorage.getItem('sessionId');
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
 
 db.ref("session/" + id + "/").on("child_added", function(data) {
+    if (data.key != "id") {
+        document.getElementById('user_1').innerHTML = data.key;
+    }
+
     console.log(data.key);
     console.log(data.val());
 });
