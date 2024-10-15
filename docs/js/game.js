56ca54809cae015b308d5d6bd273cb34ce6b5179
Michea Colautti <61267757+MicheaColautti@users.noreply.github.com>
2024-10-15 13:41:45 +0200

    Differenciate IOS and Android Sensors

--- docs/js/game.js
+++ docs/js/game.js
@@ -1,25 +1,47 @@
+
+detectOs();
 function blockInput() {
     document.getElementById("btn_jump").disabled = !document.getElementById("btn_jump").disabled;
 }
 
-function getOS() {
+function detectOs() {
     var userAgent = window.navigator.userAgent,
         platform = window.navigator.platform,
         iosPlatforms = ['iPhone', 'iPad', 'iPod'],
         os = null;
 
+
     if (iosPlatforms.indexOf(platform) !== -1) {
         os = 'iOS';
+
+        window.addEventListener("devicemotion", handleMotionIos, true);
+
+    }else{
+        os = 'Android';
+
+        window.addEventListener("devicemotion", handleMotionAndroid, true);
+
+
     }
     document.getElementById("os").innerHTML = "Dispositivi IOS non supportati. <br>Usa il tasto Jump";
 }
 
-window.addEventListener("devicemotion", handleMotion, true);
 
 
-function handleMotion(event) {
+function handleMotionIos(event) {
     var itd = getIsTouchingDown();
     var acc = event.acceleration.y;
+    alert(acc);
+    if (acc > 10 && itd) {
+        jump();
+
+    }
+}
+
+function handleMotionAndroid(event) {
+    var itd = getIsTouchingDown();
+    var acc = event.acceleration.z;
+    alert(acc);
     if (acc > 10 && itd) {
         jump();
 
