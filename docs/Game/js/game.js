5e66daaa9049ce977d4e99bd6ccea73042cd2006
Michea Colautti <61267757+MicheaColautti@users.noreply.github.com>
2024-10-17 13:45:09 +0200

    Try fix dino color

--- docs/Game/js/game.js
+++ docs/Game/js/game.js
@@ -158,8 +158,6 @@ var checkFirst = false;
             diniJumps.push(false);
             gameRef.scene.restart();
         }
-    }else{
-        checkFirst = true;
     }
 });
 
@@ -764,10 +762,17 @@ function backToHome() {
  * La funzione ritorna il colore del dino
  */
  await function getDinoColorNew(id) {
-    return db.ref('user/' + id).once('value').then(function(snapshot) {
-        console.log(snapshot.val().dino_color);
-        color=snapshot.val().dino_color;
-        console.log("COlor: "+color)
-        return color;
-    });
+     if(checkFirst) {
+
+
+         return db.ref('user/' + id).once('value').then(function (snapshot) {
+             console.log(snapshot.val().dino_color);
+             color = snapshot.val().dino_color;
+             console.log("COlor: " + color)
+             return color;
+         });
+     }else {
+         checkFirst=false;
+         return ;
+     }
 }
